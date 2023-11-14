2856ceea52f5692b9fe6d212ad77decc11926390
Precious <preciouspreccy325@gmail.com>
2023-11-15 00:44:42 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/zerotoherotest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zerotoherotest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/logic/cubit/internet_state.dart
@@ -0,0 +1,13 @@
+import '../../constants/enums.dart';
+
+abstract class InternetState {}
+
+class InternetLoading extends InternetState {}
+
+class InternetConnected extends InternetState {
+  final ConnectionType connectionType;
+
+  InternetConnected({required this.connectionType});
+}
+
+class InternetDisconnected extends InternetState {}
